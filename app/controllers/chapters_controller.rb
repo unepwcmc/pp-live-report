65615263c0bf342523918c7c77d1b46e83e767a3
stacytalbot <stacymtalbot@gmail.com>
2018-10-16 16:21:52 +0100

    Create chapter pages

---
+++ app/controllers/chapters_controller.rb
@@ -0,0 +1,42 @@
+class ChaptersController < ApplicationController
+
+  def chapter_1
+    
+  end
+
+  def chapter_2
+    
+  end
+
+  def chapter_3
+    
+  end
+
+  def chapter_4
+    
+  end
+
+  def chapter_5
+    
+  end
+
+  def chapter_6
+    
+  end
+
+  def chapter_7
+    
+  end
+
+  def chapter_8
+    
+  end
+
+  def chapter_9
+    
+  end
+
+  def chapter_10
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'  
+
+  get '/chapter-1', to: 'chapters#chapter_1'
+  get '/chapter-2', to: 'chapters#chapter_2'
+  get '/chapter-3', to: 'chapters#chapter_3'
+  get '/chapter-4', to: 'chapters#chapter_4'
+  get '/chapter-5', to: 'chapters#chapter_5'
+  get '/chapter-6', to: 'chapters#chapter_6'
+  get '/chapter-7', to: 'chapters#chapter_7'
+  get '/chapter-8', to: 'chapters#chapter_8'
+  get '/chapter-9', to: 'chapters#chapter_9'
+  get '/chapter-10', to: 'chapters#chapter_10'
 end

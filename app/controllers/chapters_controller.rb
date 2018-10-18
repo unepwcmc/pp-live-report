587259bf4d4d9cb7387ead29659553bf93037a1e
stacytalbot <stacymtalbot@gmail.com>
2018-10-18 15:16:39 +0100

    Add basic pagination component

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -3,50 +3,68 @@ class ChaptersController < ApplicationController
 
   def chapter_1
     @chapter_number = 1
+    @next_chapter_title = YAML.load(File.open("#{Rails.root}/lib/data/content/chapter-2.yml", 'r'))['title']
+    @next_chapter_link = chapter_2_path
     @data = YAML.load(File.open("#{Rails.root}/lib/data/content/chapter-1.yml", 'r'))
   end
 
   def chapter_2
     @chapter_number = 2
+    @next_chapter_title = YAML.load(File.open("#{Rails.root}/lib/data/content/chapter-3.yml", 'r'))['title']
+    @next_chapter_link = chapter_3_path
     @data = YAML.load(File.open("#{Rails.root}/lib/data/content/chapter-2.yml", 'r'))
   end
 
   def chapter_3
     @chapter_number = 3
+    @next_chapter_title = YAML.load(File.open("#{Rails.root}/lib/data/content/chapter-4.yml", 'r'))['title']
+    @next_chapter_link = chapter_4_path
     @data = YAML.load(File.open("#{Rails.root}/lib/data/content/chapter-3.yml", 'r'))
   end
 
   def chapter_4
     @chapter_number = 4
+    @next_chapter_title = YAML.load(File.open("#{Rails.root}/lib/data/content/chapter-5.yml", 'r'))['title']
+    @next_chapter_link = chapter_5_path
     @data = YAML.load(File.open("#{Rails.root}/lib/data/content/chapter-4.yml", 'r'))
   end
 
   def chapter_5
     @chapter_number = 5
+    @next_chapter_title = YAML.load(File.open("#{Rails.root}/lib/data/content/chapter-6.yml", 'r'))['title']
+    @next_chapter_link = chapter_6_path
     @data = YAML.load(File.open("#{Rails.root}/lib/data/content/chapter-5.yml", 'r'))
   end
 
   def chapter_6
     @chapter_number = 6
+    @next_chapter_title = YAML.load(File.open("#{Rails.root}/lib/data/content/chapter-7.yml", 'r'))['title']
+    @next_chapter_link = chapter_7_path
     @data = YAML.load(File.open("#{Rails.root}/lib/data/content/chapter-6.yml", 'r'))
   end
 
   def chapter_7
     @chapter_number = 7
+    @next_chapter_title = YAML.load(File.open("#{Rails.root}/lib/data/content/chapter-8.yml", 'r'))['title']
+    @next_chapter_link = chapter_8_path
     @data = YAML.load(File.open("#{Rails.root}/lib/data/content/chapter-7.yml", 'r'))
   end
 
   def chapter_8
     @chapter_number = 8
+    @next_chapter_title = YAML.load(File.open("#{Rails.root}/lib/data/content/chapter-9.yml", 'r'))['title']
+    @next_chapter_link = chapter_9_path
     @data = YAML.load(File.open("#{Rails.root}/lib/data/content/chapter-8.yml", 'r'))
   end
 
   def chapter_9
     @chapter_number = 9
+    @next_chapter_title = YAML.load(File.open("#{Rails.root}/lib/data/content/chapter-10.yml", 'r'))['title']
+    @next_chapter_link = chapter_10_path
     @data = YAML.load(File.open("#{Rails.root}/lib/data/content/chapter-9.yml", 'r'))
   end
 
-  def chapter_10
+  def chater_10
     @chapter_number = 10
     @data = YAML.load(File.open("#{Rails.root}/lib/data/content/chapter-10.yml", 'r'))
   end

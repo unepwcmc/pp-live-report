73f6090004f26b9c1ce662be06985391d96d4376
stacytalbot <stacymtalbot@gmail.com>
2018-11-15 16:21:16 +0000

    Update the site title with the page title as well

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,6 +11,10 @@ module ApplicationHelper
     content_for?(:title) ? content_for(:title) : site_title
   end
 
+  def title_meta_tag(page_title)
+    page_title == nil ? site_title: "#{page_title} | #{site_title}"
+  end
+
   def url_encode text
     ERB::Util.url_encode(text)
   end

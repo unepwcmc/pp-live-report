a97073ef936795aa75170e17200632a5bc667a4e
stacytalbot <stacymtalbot@gmail.com>
2018-11-29 10:13:16 +0000

    update data for inforgraphic maps

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -274,23 +274,16 @@ class ChaptersController < ApplicationController
     @data = YAML.load(File.open("#{Rails.root}/lib/data/content/chapter-5.yml", 'r'))
 
     @map = {
-      countries: [["USA",30],["FRA",63], ["RUS",6], ["CHN",6]],
+      data: {
+        includeDeficiant: true,
+        countries: [['USA', 1], ['FRA', 1], ['RUS', 2], ['CHN', 2], ['PAK', 2], ['PRK', 3], ['PRC', 3],  ['IND', 3], ['GBR', 4]]
+      },
       legend: [
-        {
-          title: 'No Assessments'
-        },
-        {
-          title: 'Under 10%'
-        },
-        {
-          title: '10% - 30%'
-        },
-        {
-          title: '30% - 60%'
-        },
-        {
-          title: 'Over 60%'
-        }
+        { title: 'No Assessments'},
+        { title: 'Under 10%'},
+        { title: '10% - 30%'},
+        { title: '30% - 60%'},
+        { title: 'Over 60%'}
       ]
     }
 
@@ -505,53 +498,33 @@ class ChaptersController < ApplicationController
     @data = YAML.load(File.open("#{Rails.root}/lib/data/content/chapter-7.yml", 'r'))
 
     @map_1 = {
-      countries: [["USA",30],["FRA",63], ["RUS",6], ["CHN",12]],
+      data: {
+        includeDeficiant: true,
+        countries: [['USA', 1], ['FRA', 1], ['RUS', 2], ['CHN', 2], ['PAK', 2], ['PRK', 3], ['PRC', 3],  ['IND', 3], ['GBR', 4]]
+      },
       legend: [
-        {
-          title: 'Under 4%'
-        },
-        {
-          title: '4% - 8%'
-        },
-        {
-          title: '8% - 12%'
-        },
-        {
-          title: '12% - 17%'
-        },
-        {
-          title: '17% - 25%'
-        },
-        {
-          title: 'Over 25%'
-        }
+        { title: 'Under 4%'},
+        { title: '4% - 8%'},
+        { title: '8% - 12%'},
+        { title: '12% - 17%'},
+        { title: '17% - 25%'},
+        { title: 'Over 25%'}
       ]
     }
 
     @map_2 = {
-      countries: [["USA",30],["FRA",63], ["RUS",6], ["CHN",12]],
+      data: {
+        includeDeficiant: true,
+        countries: [['USA', 1], ['FRA', 2], ['RUS', 3], ['CHN', 4], ['PAK', 5], ['PRK', 6], ['PRC', 7],  ['IND', 7], ['GBR', 7]]
+      },
       legend: [
-        {
-          title: 'A1. General increase of PA coverage'
-        },
-        {
-          title: 'A2. Targeted designation of connecting PAs'
-        },
-        {
-          title: 'B1 + B2. Permeability of unprotected lands (B1) and coordinated management of adjacent PAs (B2)'
-        },
-        {
-          title: 'B1. Permeability of unprotected lands between PAs'
-        },
-        {
-          title: 'B2. Coordinated management of adjacent PAs in the country'
-        },
-        {
-          title: 'B3. No specific priority other than PA management effectiveness for connectivity'
-        },
-        {
-          title: 'C. Coordinated management of transboundary PA linkages'
-        }
+        { title: 'A1. General increase of PA coverage' },
+        { title: 'A2. Targeted designation of connecting PAs' },
+        { title: 'B1 + B2. Permeability of unprotected lands (B1) and coordinated management of adjacent PAs (B2)' },
+        { title: 'B1. Permeability of unprotected lands between PAs' },
+        { title: 'B2. Coordinated management of adjacent PAs in the country' },
+        { title: 'B3. No specific priority other than PA management effectiveness for connectivity' },
+        { title: 'C. Coordinated management of transboundary PA linkages' }
       ]
     }
   end

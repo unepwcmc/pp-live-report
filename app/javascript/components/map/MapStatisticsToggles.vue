30a3323fd6be624b08aba8016e69a85ab6a4f7b3
stacytalbot <stacymtalbot@gmail.com>
2019-04-01 11:59:07 +0100

    Fix up map toggle styling so that it doesn't break in IE

--- app/javascript/components/map/MapStatisticsToggles.vue
+++ app/javascript/components/map/MapStatisticsToggles.vue
@@ -2,20 +2,24 @@
   <div>
     <div v-for="layer in layers" class="map__panel-layer">
       <map-statistics-toggle :map-id="mapId" :parent-tab-id="parentTabId" :ids="getMapboxLayerIds(layer)">
-        <p class="map__panel-layer-button">
-          <span class="map__panel-layer-button-inner" :style="{ 'background-color': layer.colour }"></span>
-        </p>    
+        <div class="map__panel-button-wrapper">
+          <p class="map__panel-layer-button">
+            <span class="map__panel-layer-button-inner" :style="{ 'background-color': layer.colour }"></span>
+          </p>
+        </div>
         <span v-if="layer.text_large" class="map__panel-layer-text-large">{{ layer.text_large }}</span>
-        <span>{{ layer.text_small }}</span>
+        <span class="map__panel-layer-text">{{ layer.text_small }}</span>
       </map-statistics-toggle>
 
       <template v-if="layer.sublayers">
         <map-statistics-toggle v-for="sublayer in layer.sublayers" :map-id="mapId" :parent-tab-id="parentTabId" :ids="getMapboxLayerIds(sublayer)" class="map__panel-sublayer">
-          <p class="map__panel-sublayer-button">
-            <span class="map__panel-sublayer-button-inner" :style="{ 'background-color': sublayer.colour }"></span>
-          </p>
+          <div class="map__panel-button-wrapper">
+            <p class="map__panel-sublayer-button">
+              <span class="map__panel-sublayer-button-inner" :style="{ 'background-color': sublayer.colour }"></span>
+            </p>
+          </div>
           <span class="map__panel-sublayer-text-large">{{ sublayer.text_large }}</span>
-          <span>{{ sublayer.text_small }}</span>
+          <span class="map__panel-layer-text">{{ sublayer.text_small }}</span>
         </map-statistics-toggle>
       </template>
     </div>

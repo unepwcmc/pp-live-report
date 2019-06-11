1c283c581293a45a3839da2ba708e24bed7a7cd9
Will Kocur <will.kocur@unep-wcmc.org>
2019-06-11 16:23:38 +0100

    map percentages to 2dp

--- app/serializers/global_monthly_stats_serializer.rb
+++ app/serializers/global_monthly_stats_serializer.rb
@@ -1,5 +1,5 @@
 class GlobalMonthlyStatsSerializer
-  
+
   def initialize(data)
     @data = data
   end
@@ -9,7 +9,20 @@ class GlobalMonthlyStatsSerializer
 
     @data['last_updated'] = last_updated.strftime('%B %Y')
     @data['last_updated_year'] = last_updated.strftime('%Y')
-    
+    map_coverage_percentages_to_2_dp
+
     @data
   end
+
+  private
+    def map_coverage_percentages_to_2_dp
+      [
+        'national_waters_pa_coverage_percentage',
+        'total_ocean_pa_coverage_percentage',
+        'total_land_pa_coverage_percentage',
+        'high_seas_pa_coverage_percentage'
+      ].each do |key|
+        @data[key] = "%.2f" % @data[key]
+      end
+    end
 end

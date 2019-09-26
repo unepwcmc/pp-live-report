a7653b339ac83c50ef8a94404b320b17fd80bbfb
Will Kocur <will.kocur@unep-wcmc.org>
2019-09-26 09:43:32 +0100

    add event tracking to download

---
+++ app/javascript/components/download/Download.vue
@@ -0,0 +1,38 @@
+<template>
+  <button 
+    title="Download the full report PDF" 
+    class="button--download"
+    @click="onClick"
+  >
+    <i class="icon--download"></i>
+    <span class="button--download__text">{{ text }}</span>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: ''
+    },
+    eventElement: {
+      type: String,
+      default: ''
+    },
+    downloadUrl: {
+      type: String,
+      required: true
+    }
+  },
+
+  methods: {
+    onClick () {
+      if (this.$ga) {
+        this.$ga.event(this.eventElement, 'Download')
+      }
+      window.open(this.downloadUrl, '_blank')
+    }
+  }
+}
+</script>

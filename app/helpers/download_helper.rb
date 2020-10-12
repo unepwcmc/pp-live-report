d291138da8ba13b3c4d0d3b1766da9e89bf24122
Stanley Liu <sliu1993@hotmail.co.uk>
2020-10-12 17:07:18 +0100

    added todo to download helper to be revisited

--- app/helpers/download_helper.rb
+++ app/helpers/download_helper.rb
@@ -1,5 +1,6 @@
 module DownloadHelper
   def download_links_array
+    # TODO - to_json renders a string...why? Not rendering an array properly
     [
       { url: 'pdf/Protected_Planet_Report_2018.pdf', title: '2018 report' }
     ].to_json

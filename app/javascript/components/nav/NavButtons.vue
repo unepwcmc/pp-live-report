4db631e2e197d43c6fc43e475b658c0068b3540a
Stanley Liu <sliu1993@hotmail.co.uk>
2020-10-14 15:34:11 +0100

    basic structure of navigation buttons component

---
+++ app/javascript/components/nav/NavButtons.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="nav__sticky">
+    <div>
+      <button class="nav__previous"></button>
+    </div>
+    <div>
+      <button class="nav__next"></button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+

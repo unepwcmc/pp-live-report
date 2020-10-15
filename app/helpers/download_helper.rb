e2eb69f07282e24232c17b521755d12d1d348bdb
Stanley Liu <sliu1993@hotmail.co.uk>
2020-10-15 12:27:01 +0100

    list of files is now sorted in descending order

--- app/helpers/download_helper.rb
+++ app/helpers/download_helper.rb
@@ -2,7 +2,7 @@ module DownloadHelper
   PDF_DIR = 'public'
 
   def download_links_array
-    Dir.glob('**/*.pdf', base: PDF_DIR).each do |pdf|
+    Dir.glob('**/*.pdf', base: PDF_DIR).sort.reverse.map do |pdf|
       {
         url: pdf,
         title: "#{pdf.match(/\d+/)[0]} Report"

43ff79dae18294b1be131a76ad1c72e6914d1ae9
Stanley Liu <sliu1993@hotmail.co.uk>
2020-10-20 11:00:07 +0100

    added wdpa and oecm to legend

--- app/javascript/components/charts/ChartRow.vue
+++ app/javascript/components/charts/ChartRow.vue
@@ -1,34 +1,60 @@
 <template>
-  <div class="chart--row">
-    <p v-if="title" class="chart__title">{{ title }}</p>
+ <div class="chart--row">
+  <p v-if="title" class="chart__title">{{ title }}</p>
 
-    <div class="chart__chart">
-      <div v-for="(row, index) in rows" :key="`row-${index}`" class="chart__row flex flex-v-center flex-h-between" :class="themeClass">
-        <span class="chart__bar" :style="{ width: row.percent + '%' }"></span> 
-        <span class="chart__percent">{{ row.percent }}%</span>
-        <span class="chart__label">{{ row.label }}</span>
-      </div>
-    </div>
+  <div class="chart__chart">
+   <div
+    v-for="(row, index) in rows"
+    :key="`row-${index}`"
+    class="chart__row flex flex-v-center flex-h-between"
+    :class="themeClass"
+   >
+    <span class="chart__bar" :style="{ width: row.percent + '%' }"></span>
+    <span class="chart__percent">{{ row.percent }}%</span>
+    <span class="chart__label">{{ row.label }}</span>
+   </div>
   </div>
+  <ul class="chart__legend">
+   <li>
+    <span :class="`chart-key__oecm--${legendClass}`"></span>
+    <p>OECMs {{ oecmPercent }}%</p>
+   </li>
+   <li>
+    <span :class="`chart-key__wdpa--${legendClass}`"></span>
+    <p>WDPAs {{ wdpaPercent }}%</p>
+   </li>
+  </ul>
+ </div>
 </template>
 
 <script>
-  export default {
-    name: 'chart-row',
+export default {
+ name: "chart-row",
 
-    props: {
-      title: String,
-      theme: String,
-      rows: {
-        type: Array,
-        required: true
-      }
-    },
+ props: {
+  oecmPercent: {
+   type: Number,
+   default: 0,
+  },
+  title: String,
+  theme: String,
+  rows: {
+   type: Array,
+   required: true,
+  },
+  wdpaPercent: {
+   type: Number,
+   default: 0,
+  },
+ },
 
-    computed: {
-      themeClass () {
-        return `chart-theme--${this.theme}`
-      }
-    }
+ computed: {
+  themeClass() {
+   return `chart-theme--${this.theme}`;
+  },
+  legendClass() {
+    return `${this.theme}`;
   }
+ },
+};
 </script>

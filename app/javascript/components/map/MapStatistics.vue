546d0e7b66ac4cdbf7578632b88e622d0273470b
Stanley Liu <sliu1993@hotmail.co.uk>
2020-10-26 16:04:49 +0000

    map panel can now be toggled

--- app/javascript/components/map/MapStatistics.vue
+++ app/javascript/components/map/MapStatistics.vue
@@ -1,208 +1,245 @@
 <template>
-  <div class="map--statistics">
-    <div :id="id" class="map__map"></div>
+ <div class="map--statistics">
+  <div :id="id" class="map__map"></div>
 
-    <div class="map__panel gutters">
-      <h2 class="heading--map">{{ title }}</h2>
-      <p v-if="description" class="map__panel-description">{{ description }}</p>
-      
-      <template v-if="tabs">
-        <tabs :id="`tabs-${id}`">
-          <tab v-for="(tab, index) in tabs" :key="`tab-${index}`" :id="getTabId(index)" :title="tab.title">
-            <map-statistics-toggles :map-id="id" :parent-tab-id="getTabId(index)" :layers="tab.layers"></map-statistics-toggles>
-          </tab>
-        </tabs>
-      </template>
+  <div class="map__panel gutters">
+   <div class="map__panel-top">
+    <h2 class="heading--map">{{ title }}</h2>
+    <i
+     :class="[isActive ? 'icon--close-white' : 'icon--minus-white']"
+     @click="togglePanel"
+    ></i>
+   </div>
+   <p v-if="description" class="map__panel-description">{{ description }}</p>
 
-      <template v-else>
-        <map-statistics-toggles :map-id="id" :layers="layers"></map-statistics-toggles>
-      </template>
-
-    </div>
-    <div class="map__smallprint container--medium">
-      <p class="smallprint">{{ source }}</p>
-      <p class="smallprint">{{ disclaimer }}</p>
-    </div>
+   <template v-if="isActive">
+    <template v-if="tabs">
+     <tabs :id="`tabs-${id}`">
+      <tab
+       v-for="(tab, index) in tabs"
+       :key="`tab-${index}`"
+       :id="getTabId(index)"
+       :title="tab.title"
+      >
+       <map-statistics-toggles
+        :map-id="id"
+        :parent-tab-id="getTabId(index)"
+        :layers="tab.layers"
+       ></map-statistics-toggles>
+      </tab>
+     </tabs>
+    </template>
+    <template v-else>
+     <map-statistics-toggles
+      :map-id="id"
+      :layers="layers"
+     ></map-statistics-toggles>
+    </template>
+   </template>
   </div>
+  <div class="map__smallprint container--medium">
+   <p class="smallprint">{{ source }}</p>
+   <p class="smallprint">{{ disclaimer }}</p>
+  </div>
+ </div>
 </template>
 
 <script>
-import { getMapboxLayerId, getLayerIdFromMapboxLayerId, getFirstTopLayerId } from './map-helpers'
-import MapStatisticsToggles from './MapStatisticsToggles'
-import Tab from '../tabs/Tab'
-import Tabs from '../tabs/Tabs'
+import {
+ getMapboxLayerId,
+ getLayerIdFromMapboxLayerId,
+ getFirstTopLayerId,
+} from "./map-helpers";
+import MapStatisticsToggles from "./MapStatisticsToggles";
+import Tab from "../tabs/Tab";
+import Tabs from "../tabs/Tabs";
 
 import { eventHub } from "../../packs/application.js";
 
 export default {
-  name: 'map-statistics',
+ name: "map-statistics",
 
-  components: { MapStatisticsToggles, Tab, Tabs },
+ components: { MapStatisticsToggles, Tab, Tabs },
 
-  props: {
-    id: {
-      type: String,
-      required: true
-    },
-    title: String,
-    description: String,
-    tabs: Array,
-    layers: Array,
-    tilesUrl: String,
-    source: String,
-    disclaimer: String
+ props: {
+  id: {
+   type: String,
+   required: true,
+  },
+  title: String,
+  description: String,
+  tabs: Array,
+  layers: Array,
+  tilesUrl: String,
+  source: String,
+  disclaimer: String,
+ },
+
+ data() {
+  return {
+   map: {},
+   isActive: true,
+   mapboxToken: process.env.MAPBOX_TOKEN,
+   allLayers: [],
+   firstTopLayerId: "",
+  };
+ },
+
+ computed: {
+  initialLayers() {
+   return this.tabs ? this.tabs[0].layers : this.allLayers;
+  },
+ },
+
+ mounted() {
+  eventHub.$on("hideLayers", this.hideLayers);
+  eventHub.$on("showLayers", this.showLayers);
+  this.getAllLayers();
+  this.createMap();
+ },
+
+ methods: {
+  getTabId(index) {
+   return `tab-${index}`;
   },
 
-  data () {
-    return {
-      map: {},
-      mapboxToken: process.env.MAPBOX_TOKEN,
-      allLayers: [],
-      firstTopLayerId: ''
+  getLayerIdFromMapboxLayerId(mapboxLayerId) {
+   return getLayerIdFromMapboxLayerId(mapboxLayerId);
+  },
+
+  getAllLayers() {
+   if (this.tabs) {
+    this.tabs.forEach((tab) => {
+     this.allLayers = this.allLayers.concat(tab.layers);
+    });
+   } else {
+    this.allLayers = this.allLayers.concat(this.layers);
+   }
+  },
+
+  getLayerById(id) {
+   let layer = null;
+
+   this.allLayers.forEach((l) => {
+    if (l.id === id) {
+     layer = l;
     }
+   });
+
+   return layer;
   },
 
-  computed: {
-    initialLayers () {
-      return this.tabs ? this.tabs[0].layers : this.allLayers
+  createMap() {
+   mapboxgl.accessToken = this.mapboxToken;
+
+   this.map = new mapboxgl.Map({
+    container: this.id,
+    style: "mapbox://styles/unepwcmc/cjo95gdrg0qzh2roan77pelcj",
+    center: [0.0, -0.0],
+    zoom: 1.3,
+   });
+
+   this.map.scrollZoom.disable();
+   this.map.addControl(
+    new mapboxgl.NavigationControl({ showCompass: false }),
+    "top-left"
+   );
+   this.map.on("load", () => {
+    this.firstTopLayerId = getFirstTopLayerId(this.map);
+    this.addInitialLayers();
+   });
+  },
+
+  addInitialLayers() {
+   if (this.initialLayers.length) {
+    this.initialLayers.forEach((layer) => {
+     this.addLayerAndSubLayers(layer);
+    });
+   }
+  },
+
+  addLayerAndSubLayers(layer) {
+   this.addLayer(layer);
+
+   if (layer.sublayers) {
+    layer.sublayers.forEach((sublayer) => {
+     this.addLayer(sublayer);
+    });
+   }
+  },
+
+  addLayer(layer) {
+   if (layer.source_layers && this.tilesUrl) {
+    Object.keys(layer.source_layers).forEach((layerType) => {
+     this.addMapboxLayer({
+      tiles: [this.tilesUrl],
+      layerType,
+      layer,
+     });
+    });
+   }
+  },
+
+  addMapboxLayer({ tiles, layerType, layer }) {
+   const options = {
+    id: getMapboxLayerId(layer, layerType),
+    source: {
+     type: "vector",
+     tiles: tiles,
+    },
+    "source-layer": layer.source_layers[layerType],
+    filter: layer.filter_id ? ["==", "_symbol", layer.filter_id] : ["all"],
+   };
+
+   if (layerType === "point") {
+    options["type"] = "circle";
+    options["paint"] = {
+     "circle-radius": 2,
+     "circle-color": layer.colour,
+    };
+   } else {
+    options["type"] = "fill";
+    options["paint"] = {
+     "fill-color": layer.colour,
+     "fill-outline-color": "transparent",
+    };
+   }
+
+   this.map.addLayer(options, this.firstTopLayerId);
+  },
+
+  hideLayers(ids) {
+   this.setVisibilityOfLayers(ids, false);
+  },
+
+  showLayers(ids) {
+   this.setVisibilityOfLayers(ids, true);
+  },
+
+  setVisibilityOfLayers(ids, isVisible) {
+   if (ids.mapId !== this.id) {
+    return;
+   }
+
+   const newVisibility = isVisible ? "visible" : "none";
+
+   ids.layerIds.forEach((mapboxLayerId) => {
+    if (this.map.getLayer(mapboxLayerId)) {
+     this.map.setLayoutProperty(mapboxLayerId, "visibility", newVisibility);
+    } else if (isVisible) {
+     const baseLayer = this.getLayerById(
+      this.getLayerIdFromMapboxLayerId(mapboxLayerId)
+     );
+
+     if (baseLayer) {
+      this.addLayer(baseLayer);
+     }
     }
+   });
   },
-
-  mounted () {
-    eventHub.$on('hideLayers', this.hideLayers)
-    eventHub.$on('showLayers', this.showLayers)
-    this.getAllLayers()
-    this.createMap()
+  togglePanel() {
+   this.isActive = !this.isActive;
   },
-
-  methods: {
-    getTabId (index) {
-      return `tab-${index}`
-    },
-
-    getLayerIdFromMapboxLayerId (mapboxLayerId) {
-      return getLayerIdFromMapboxLayerId(mapboxLayerId)
-    },
-
-    getAllLayers () {
-      if(this.tabs) {
-        this.tabs.forEach(tab => {
-          this.allLayers = this.allLayers.concat(tab.layers)
-        })
-      } else {
-        this.allLayers = this.allLayers.concat(this.layers)
-      }
-    },
-
-    getLayerById (id) {
-      let layer = null
-
-      this.allLayers.forEach(l => {
-        if (l.id === id) { layer = l }
-      })
-
-      return layer
-    },
-
-    createMap () {
-      mapboxgl.accessToken = this.mapboxToken
-
-      this.map = new mapboxgl.Map({
-        container: this.id,
-        style: 'mapbox://styles/unepwcmc/cjo95gdrg0qzh2roan77pelcj',
-        center: [0.000000, -0.000000],
-        zoom: 1.3
-      })
-
-      this.map.scrollZoom.disable()
-      this.map.addControl(new mapboxgl.NavigationControl({ showCompass: false }), 'top-left')
-      this.map.on("load", () => { 
-        this.firstTopLayerId = getFirstTopLayerId(this.map)
-        this.addInitialLayers() 
-      })
-    },
-
-    addInitialLayers () {
-      if (this.initialLayers.length) {
-        this.initialLayers.forEach(layer => { 
-          this.addLayerAndSubLayers(layer) 
-        })
-      }
-    },
-
-    addLayerAndSubLayers (layer) {
-      this.addLayer(layer)
-
-      if(layer.sublayers) {
-        layer.sublayers.forEach(sublayer => {
-          this.addLayer(sublayer)
-        })
-      }
-    },
-
-    addLayer (layer) {  
-      if (layer.source_layers && this.tilesUrl) {
-        Object.keys(layer.source_layers).forEach(layerType => {
-          this.addMapboxLayer({
-            tiles: [this.tilesUrl],
-            layerType,
-            layer
-          })
-        })
-      }
-    },
-
-    addMapboxLayer ({tiles, layerType, layer}) {
-      const options = {
-        'id': getMapboxLayerId(layer, layerType),
-        'source': {
-          'type': 'vector',
-          'tiles': tiles
-        },
-        'source-layer': layer.source_layers[layerType],
-        'filter': layer.filter_id ? ['==', '_symbol', layer.filter_id] : ['all']
-      }
-
-      if (layerType === 'point') {
-        options['type'] = 'circle'
-        options['paint'] = { 
-          'circle-radius': 2,
-          'circle-color': layer.colour,
-        }
-      } else {
-        options['type'] = 'fill'
-        options['paint'] = {
-          'fill-color': layer.colour,
-          'fill-outline-color': 'transparent'
-        }
-      }
-
-      this.map.addLayer(options, this.firstTopLayerId)
-    },
-
-    hideLayers (ids) {
-      this.setVisibilityOfLayers(ids, false)
-    },
-
-    showLayers (ids) {
-      this.setVisibilityOfLayers(ids, true)
-    },
-
-    setVisibilityOfLayers (ids, isVisible) {
-      if (ids.mapId !== this.id) { return }
-
-      const newVisibility = isVisible ? 'visible' : 'none'
-
-      ids.layerIds.forEach(mapboxLayerId => {
-        if(this.map.getLayer(mapboxLayerId)) {
-          this.map.setLayoutProperty(mapboxLayerId, "visibility", newVisibility);
-        } else if (isVisible) {
-          const baseLayer = this.getLayerById(this.getLayerIdFromMapboxLayerId(mapboxLayerId))
-
-          if(baseLayer) { this.addLayer(baseLayer) }
-        }
-      })
-    }
-  }
-}
+ },
+};
 </script>

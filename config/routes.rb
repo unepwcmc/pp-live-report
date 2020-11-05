0c380b1d2fb3d14e65bce65c41e5f1724321c21d
Stanley Liu <sliu1993@hotmail.co.uk>
2020-11-05 17:42:22 +0000

    refactored routes to accommodate any number of chapters

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'  
 
-  get '/chapter-1', to: 'chapters#chapter_1'
-  get '/chapter-2', to: 'chapters#chapter_2'
-  get '/chapter-3', to: 'chapters#chapter_3'
-  get '/chapter-4', to: 'chapters#chapter_4'
-  get '/chapter-5', to: 'chapters#chapter_5'
-  get '/chapter-6', to: 'chapters#chapter_6'
-  get '/chapter-7', to: 'chapters#chapter_7'
-  get '/chapter-8', to: 'chapters#chapter_8'
-  get '/chapter-9', to: 'chapters#chapter_9'
-  get '/chapter-10', to: 'chapters#chapter_10'
+  # Irrespective of how many view files are present in the /chapters directory
+  Dir.children('app/views/chapters').each do |i|
+    name = File.basename(i, ".html.erb")
+    get "/#{name.gsub(/_/, '-')}", to: "chapters##{name}"
+  end
 end

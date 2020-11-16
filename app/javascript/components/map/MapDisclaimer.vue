ce5fd32f7ae89dbb17eafbe912b0ea3324bad96c
Stanley Liu <sliu1993@hotmail.co.uk>
2020-11-16 14:26:34 +0000

    added modal component in

--- app/javascript/components/map/MapDisclaimer.vue
+++ app/javascript/components/map/MapDisclaimer.vue
@@ -1,19 +1,38 @@
 <template>
  <div>
-  <button class="button--grey">
+  <button class="button--grey" @click="openModal">
    <span class="button--grey__text">Map disclaimer</span>
   </button>
-  <!-- Modal component needs to go here for map disclaimer -->
+  <modal
+   :text="disclaimer"
+   :is-active="isActive"
+   @close-modal="closeModal"
+  ></modal>
  </div>
 </template>
 
 <script>
+import Modal from '../modal/Modal.vue'
+
 export default {
  name: "MapDisclaimer",
  props: {
   disclaimer: {
    type: Object,
    default: () => ({}),
+  },
+ },
+ data() {
+  return {
+   isActive: false
+  }
+ },
+ methods: {
+  openModal() {
+   this.isActive = true
+  },
+  closeModal() {
+   this.isActive = false
   },
  },
 };

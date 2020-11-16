82a949965ad860cbf9622fcc14c766a5e3de02a4
Stanley Liu <sliu1993@hotmail.co.uk>
2020-11-16 11:00:37 +0000

    year taken out of bars and placed above each bar

--- app/javascript/components/charts/ChartRow.vue
+++ app/javascript/components/charts/ChartRow.vue
@@ -3,20 +3,20 @@
   <p v-if="title" class="chart__title">{{ title }}</p>
 
   <div class="chart__chart">
-   <div
-    v-for="(row, index) in rows"
-    :key="`row-${index}`"
-    class="chart__row flex flex-v-center flex-h-between"
-    :class="themeClass"
-   >
-    <span class="chart__bar" :style="{ width: row.percent + '%' }"></span>
-    <span
-     :class="`chart__bar--oecm ${legendClass}`"
-     v-if="oecmPercent > 0"
-     :style="{ width: oecmPercent + '%', left: row.percent + '%' }"
-    ></span>
-    <span class="chart__percent">{{ totalPercent(row.percent) }}%</span>
+   <div v-for="(row, index) in rows" :key="`row-${index}`">
     <span class="chart__label">{{ row.label }}</span>
+    <div
+     class="chart__row flex flex-v-center flex-h-between"
+     :class="themeClass"
+    >
+     <span class="chart__bar" :style="{ width: row.percent + '%' }"></span>
+     <span
+      :class="`chart__bar--oecm ${legendClass}`"
+      v-if="oecmPercent > 0"
+      :style="{ width: oecmPercent + '%', left: row.percent + '%' }"
+     ></span>
+     <span class="chart__percent">{{ totalPercent(row.percent) }}%</span>
+    </div>
    </div>
   </div>
   <ul class="chart__legend">
@@ -33,7 +33,7 @@
 </template>
 
 <script>
-import ScrollMagic from "scrollmagic";
+import ScrollMagic from "scrollmagic"
 
 export default {
  name: "chart-row",
@@ -50,33 +50,33 @@ export default {
   return {
    //  TODO - set oecmPercent as key of row
    oecmPercent: 4
-  };
+  }
  },
  mounted() {
-  this.animateOnScroll();
+  this.animateOnScroll()
  },
  methods: {
   animateOnScroll() {
-   const controller = new ScrollMagic.Controller();
+   const controller = new ScrollMagic.Controller()
    const scene = new ScrollMagic.Scene({
     triggerElement: ".chart--row__subtitle",
     reverse: false,
    }).setClassToggle(
     ".chart__bar, .chart__bar--oecm",
     "bar-animate"
-   );
-   scene.addTo(controller);
+   )
+   scene.addTo(controller)
   },
   totalPercent(percent) {
-   return percent + this.oecmPercent;
+   return percent + this.oecmPercent
   },
  },
  computed: {
   themeClass() {
-   return `chart-theme--${this.theme}`;
+   return `chart-theme--${this.theme}`
   },
   legendClass() {
-   return `${this.theme}`;
+   return `${this.theme}`
   },
  },
 };

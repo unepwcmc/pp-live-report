9d509475a78da36c3eafff7c873b84ad7e1b1c72
Stanley Liu <sliu1993@hotmail.co.uk>
2020-11-17 15:05:52 +0000

    fixed issue with navigation creating chapter 12 link by accident

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -2,8 +2,8 @@ module NavigationHelper
   include YamlHelpers
 
   CONTENT_BASE_PATH = 'lib/data/content'.freeze
-  FIRST_CHAPTER = 1.freeze
-  LAST_CHAPTER = 10.freeze
+  FIRST_CHAPTER = 1
+  LAST_CHAPTER = 11
 
   def fetch_chapter_links(current_chapter)
     {
@@ -12,8 +12,8 @@ module NavigationHelper
     }
   end
 
-  def populate_chapter_json(chapter_number)
-    return if chapter_number == ( FIRST_CHAPTER - 1 || LAST_CHAPTER + 1 )
-    URI.join(root_url, "/chapter-#{chapter_number}")
+  def populate_chapter_json(number)
+    return if number == FIRST_CHAPTER - 1 || number == LAST_CHAPTER + 1
+    URI.join(root_url, "/chapter-#{number}")
   end
 end

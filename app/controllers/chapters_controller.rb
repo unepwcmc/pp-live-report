23fe4695a054202093952c507684f1eb18e7fb7e
Stacy Talbot <stacy.talbot@unep-wcmc.org>
2021-04-06 17:33:52 +0100

    Update map 2 on ch 3 with dummy data

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -135,37 +135,78 @@ class ChaptersController < ApplicationController
 
     @map_2 = {
       id: 'map_2',
-      tiles_url: 'https://tiles.arcgis.com/tiles/Mj0hjvkNtV7NRhA7/arcgis/rest/services/PP_Live_Ch2_Fg5_Oct20/VectorTileServer/tile/{z}/{y}/{x}.pbf',
-      layers: [
+      tiles_url: 'https://tiles.arcgis.com/tiles/Mj0hjvkNtV7NRhA7/arcgis/rest/services/nat_cov_pas_merc/VectorTileServer/tile/{z}/{y}/{x}.pbf',
+      tiles_url_oecm: 'https://tiles.arcgis.com/tiles/Mj0hjvkNtV7NRhA7/arcgis/rest/services/nat_cov_pas_merc_oecm/VectorTileServer/tile/{z}/{y}/{x}.pbf',
+      tabs: [
         {
-          id: 'over-ten-' + random_number,
-          text_large: 'Over 10%',
-          source_layers: { poly: 'Ch2_Fg5_mcat5' },
-          colour: PURPLE_SCHEME[3]
+          title: 'Terrestrial',
+          layers: [
+            {
+              id: 'terrestrial-data-deficient-' + random_number,
+              text_large: 'Data deficient',
+              source_layers: { poly: 'land_nat_cov_cat1' },
+              colour: DEFAULT_COLOUR
+            },
+            {
+              id: 'terrestrial-less-than-3-' + random_number,
+              text_large: 'Under 3%',
+              source_layers: { poly: 'land_nat_cov_cat2' },
+              colour: GREEN_SCHEME[0]
+            },
+            {
+              id: 'terrestrial-three-to-six-' + random_number,
+              text_large: '3% – 6%',
+              source_layers: { poly: 'land_nat_cov_cat3' },
+              colour: GREEN_SCHEME[1]
+            },
+            {
+              id: 'terrestrial-six-to-ten-' + random_number,
+              text_large: '6% - 10%',
+              source_layers: { poly: 'land_nat_cov_cat4' },
+              colour: GREEN_SCHEME[2]
+            },
+            {
+              id: 'terrestrial-over-ten-' + random_number,
+              text_large: 'Over 10%',
+              source_layers: { poly: 'land_nat_cov_cat5' },
+              colour: GREEN_SCHEME[3]
+            }
+          ]
         },
         {
-          id: 'six-to-ten-' + random_number,
-          text_large: '6% - 10%',
-          source_layers: { poly: 'Ch2_Fg5_mcat4' },
-          colour: PURPLE_SCHEME[2]
-        },
-        {
-          id: 'three-to-six-' + random_number,
-          text_large: '3% – 6%',
-          source_layers: { poly: 'Ch2_Fg5_mcat3' },
-          colour: PURPLE_SCHEME[1]
-        },
-        {
-          id: 'less-than-3-' + random_number,
-          text_large: 'Under 3%',
-          source_layers: { poly: 'Ch2_Fg5_mcat2' },
-          colour: PURPLE_SCHEME[0]
-        },
-        {
-          id: 'data-deficient-' + random_number,
-          text_large: 'Data deficient',
-          source_layers: { poly: 'Ch2_Fg5_mcat1' },
-          colour: DEFAULT_COLOUR
+          title: 'Marine',
+          layers: [
+            {
+              id: 'marine-data-deficient-' + random_number,
+              text_large: 'Data deficient',
+              source_layers: { poly: 'sea_nat_cov_cat1' },
+              colour: DEFAULT_COLOUR
+            },
+            {
+              id: 'marine-less-than-3-' + random_number,
+              text_large: 'Under 3%',
+              source_layers: { poly: 'sea_nat_cov_cat2' },
+              colour: BLUE_SCHEME[0]
+            },
+            {
+              id: 'marine-three-to-six-' + random_number,
+              text_large: '3% – 6%',
+              source_layers: { poly: 'sea_nat_cov_cat3' },
+              colour: BLUE_SCHEME[1]
+            },
+            {
+              id: 'marine-six-to-ten-' + random_number,
+              text_large: '6% - 10%',
+              source_layers: { poly: 'sea_nat_cov_cat4' },
+              colour: BLUE_SCHEME[2]
+            },
+            {
+              id: 'marine-over-ten-' + random_number,
+              text_large: 'Over 10%',
+              source_layers: { poly: 'sea_nat_cov_cat5' },
+              colour: BLUE_SCHEME[3]
+            }
+          ]
         }
       ]
     }
